423b291092cdd638551aac8a767f967a6e0fd267
fur1ozz <tomassveveris@gmail.com>
2024-10-24 00:11:55 +0300

    0.27 updated display name of a test

--- tests/Feature/SeasonTest.php
+++ tests/Feature/SeasonTest.php
@@ -10,7 +10,7 @@ use Tests\TestCase;
 class SeasonTest extends TestCase
 {
     use RefreshDatabase;
-    public function test_it_can_insert_a_season()
+    public function test_season_can_be_inserted_in_seasons_table()
     {
         $year = 2024;
 
