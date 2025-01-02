affbd4a28fe3de4b810a23e43da535a597c4c4db
fur1ozz <tomassveveris@gmail.com>
2025-01-02 20:31:53 +0200

    0.34 created splits table seeder

---
+++ database/seeders/SplitsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Split;
+use App\Models\Stage;
+use Illuminate\Database\Seeder;
+
+class SplitsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $stageIds = Stage::where('rally_id', 4)->pluck('id');
+
+        foreach ($stageIds as $stageId) {
+            $previousDistance = 0;
+
+            for ($splitNumber = 1; $splitNumber <= 5; $splitNumber++) {
+                $increment = round(mt_rand(20, 50) / 10, 1);
+                $splitDistance = $previousDistance + $increment;
+
+                Split::create([
+                    'stage_id' => $stageId,
+                    'split_number' => $splitNumber,
+                    'split_distance' => $splitDistance,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+
+                $previousDistance = $splitDistance;
+            }
+        }
+    }
+}
