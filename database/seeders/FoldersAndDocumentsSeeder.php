036f50e001c255a4788ff4c9cf534ec86f5fda1d
fur1ozz <tomassveveris@gmail.com>
2025-03-10 21:21:10 +0200

    0.44 Fix FoldersAndDocumentsSeeder

--- database/seeders/FoldersAndDocumentsSeeder.php
+++ database/seeders/FoldersAndDocumentsSeeder.php
@@ -6,89 +6,61 @@ use Illuminate\Database\Seeder;
 use App\Models\Folder;
 use App\Models\Document;
 use App\Models\Rally;
+use Illuminate\Support\Facades\DB;
 
 class FoldersAndDocumentsSeeder extends Seeder
 {
     public function run()
     {
-        $rallyId = 1;
+        $rallies = Rally::all();
 
-        $data = [
+        $foldersData = [
             [
                 "number" => "1",
                 "title" => "Nolikumi un Biļeteni / Regulations and Bulletins",
                 "files" => [
-                    [
-                        "name" => "Papildus nolikums",
-                        "link" => "https://example.com/papildus-nolikums.pdf"
-                    ],
-                    [
-                        "name" => "Supplementary Regulations",
-                        "link" => "https://example.com/supplementary-regulations.pdf"
-                    ],
-                    [
-                        "name" => "Pielikums Nr.1 - Maršruta karte | Appendix No.1 - Primary itinerary",
-                        "link" => "https://example.com/appendix-no1-itinerary.pdf"
-                    ]
+                    "Supplementary Regulations",
+                    "Rally Guidelines",
+                    "Itinerary Overview",
                 ]
             ],
             [
                 "number" => "2",
                 "title" => "Komisāru dokumenti / Stewards documents",
                 "files" => [
-                    [
-                        "name" => "Stewards Decision No.1",
-                        "link" => "https://example.com/stewards-decision-no1.pdf"
-                    ],
-                    [
-                        "name" => "Stewards Decision No.2 - Crew 36",
-                        "link" => "https://example.com/stewards-decision-no2-crew36.pdf"
-                    ]
+                    "Stewards Report No.1",
+                    "Decision Report - Crew 24",
                 ]
             ],
             [
                 "number" => "3",
-                "title" => "Sacensību vadītāja dokumenti/ CoC documents",
+                "title" => "Sacensību vadītāja dokumenti / CoC documents",
                 "files" => [
-                    [
-                        "name" => "Komunikācija Nr.2 | Communication No2 - Rallysprint",
-                        "link" => "https://app-cdn.sportity.com/51115f7b-c8e5-4371-8a2b-04897826994d/communication_no2_rallysprint.pdf"
-                    ],
-                    [
-                        "name" => "Komunikācija Nr.3 | Communication No3",
-                        "link" => "https://app-cdn.sportity.com/51115f7b-c8e5-4371-8a2b-04897826994d/communication_no3.pdf"
-                    ],
-                    [
-                        "name" => "Komunikācija Nr.4 | Communication No4",
-                        "link" => "https://app-cdn.sportity.com/51115f7b-c8e5-4371-8a2b-04897826994d/communication_no4.pdf"
-                    ],
-                    [
-                        "name" => "Komunikācija Nr.5 | Communication No5",
-                        "link" => "https://app-cdn.sportity.com/51115f7b-c8e5-4371-8a2b-04897826994d/communication_no5.pdf"
-                    ],
-                    [
-                        "name" => "Komunikācija Nr.6 | Communication No6",
-                        "link" => "https://app-cdn.sportity.com/51115f7b-c8e5-4371-8a2b-04897826994d/communication_no6.pdf"
-                    ]
+                    "Race Director's Notes No.2",
+                    "Official Rally Update No.3",
+                    "Safety Instructions",
+                    "Communication Bulletin",
+                    "Final Event Summary",
                 ]
             ]
         ];
 
-        foreach ($data as $folderData) {
-            $folder = Folder::create([
-                'rally_id' => $rallyId,
-                'number' => $folderData['number'],
-                'title' => $folderData['title']
-            ]);
-
-            foreach ($folderData['files'] as $file) {
-                Document::create([
-                    'folder_id' => $folder->id,
-                    'name' => $file['name'],
-                    'link' => $file['link']
+        foreach ($rallies as $rally) {
+            foreach ($foldersData as $folderData) {
+                $folder = Folder::create([
+                    'rally_id' => $rally->id,
+                    'number' => $folderData['number'],
+                    'title' => $folderData['title']
                 ]);
+
+                foreach ($folderData['files'] as $index => $fileName) {
+                    Document::create([
+                        'folder_id' => $folder->id,
+                        'name' => $fileName,
+                        'link' => "https://example.com/document-{$rally->id}-{$folder->number}-" . ($index + 1) . ".pdf",
+                    ]);
+                }
             }
         }
     }
 }
-
