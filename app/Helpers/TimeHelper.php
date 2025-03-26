88a43ba333ae905c916c6138081b7ece64ff4b1e
fur1ozz <tomassveveris@gmail.com>
2025-03-26 22:34:29 +0200

    2.1.03 Add adaptive time formating, that allows to specify how many numbers you want to see for milsieconds. Rewrite the split calculation logic

--- app/Helpers/TimeHelper.php
+++ app/Helpers/TimeHelper.php
@@ -3,7 +3,7 @@
 if (!function_exists('lrc_formatMillisecondsTwoDigits')) {
     /**
      * Convert milliseconds to "hours:minutes:seconds.milliseconds" format with 2-digit milliseconds.
-     * Only displays hours if time exceeds 60 minutes.
+     * - Only displays hours if time exceeds 60 minutes.
      *
      * @param int $milliseconds Time in milliseconds
      * @return string Formatted time as "h:mm:ss.ms" or "mm:ss.ms"
@@ -22,5 +22,36 @@ if (!function_exists('lrc_formatMillisecondsTwoDigits')) {
         }
 
         return sprintf('%d:%02d.%02d', $minutes, $seconds, $ms);
+    }
+}
+
+if (!function_exists('lrc_formatMillisecondsAdaptive')) {
+    /**
+     * Convert milliseconds to a dynamically formatted time string with 2-digit milliseconds.
+     * - Displays "s.ms" if under 60 seconds.
+     * - Displays "m:ss.ms" if under 60 minutes.
+     * - Displays "h:mm:ss.ms" if 1 hour or more.
+     * - The number of displayed millisecond digits (precision) can be specified (default is 2).
+     *
+     * @param int $milliseconds Time in milliseconds
+     * @param int $msPrecision Number of digits to display for milliseconds (recommended: 1-3).
+     * @return string Formatted time
+     */
+    function lrc_formatMillisecondsAdaptive(int $milliseconds, int $msPrecision = 2): string
+    {
+        $totalSeconds = floor($milliseconds / 1000);
+        $hours = floor($totalSeconds / 3600);
+        $minutes = floor(($totalSeconds % 3600) / 60);
+        $seconds = $totalSeconds % 60;
+
+        $ms = floor(($milliseconds % 1000) / pow(10, 3 - $msPrecision));
+
+        if ($totalSeconds < 60) {
+            return sprintf('%d.%0'.$msPrecision.'d', $seconds, $ms);
+        } elseif ($totalSeconds < 3600) {
+            return sprintf('%d:%02d.%0'.$msPrecision.'d', $minutes, $seconds, $ms);
+        } else {
+            return sprintf('%d:%02d:%02d.%0'.$msPrecision.'d', $hours, $minutes, $seconds, $ms);
+        }
     }
 }
